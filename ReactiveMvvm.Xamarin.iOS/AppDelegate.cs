2ae2761f01f69bbab5394d4b1b3b132df62719b0
Artjom Graf <prizrak.nra@gmail.com>
2018-09-16 21:40:50 +0300

    Add Xamarin.iOS project

---
+++ ReactiveMvvm.Xamarin.iOS/AppDelegate.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using ReactiveMvvm.Xamarin;
+using UIKit;
+using Xamarin.Forms;
+
+namespace Blank
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
+
+
---
+++ ReactiveMvvm.Xamarin.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReactiveMvvm.Xamarin.iOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReactiveMvvm.Xamarin.iOS")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("50c7b8c9-e664-45af-b88e-0c9b8b9c1be1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
