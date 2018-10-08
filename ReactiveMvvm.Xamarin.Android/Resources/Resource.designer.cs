e999aab4ea62ccecab10171af5d6ee1529f93949
Artjom Graf <prizrak.nra@gmail.com>
2018-10-08 18:31:48 +0300

    Use latest ReactiveUI 9.0.1

--- ReactiveMvvm.Xamarin.Android/Resources/Resource.designer.cs
+++ ReactiveMvvm.Xamarin.Android/Resources/Resource.designer.cs
@@ -26,7 +26,6 @@ namespace ReactiveMvvm.Xamarin.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Splat.Resource.String.library_name = global::ReactiveMvvm.Xamarin.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::ReactiveMvvm.Xamarin.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -4033,9 +4032,6 @@ namespace ReactiveMvvm.Xamarin.Droid
 			
 			// aapt resource value: 0x7f090038
 			public const int character_counter_pattern = 2131296312;
-			
-			// aapt resource value: 0x7f09003e
-			public const int library_name = 2131296318;
 			
 			// aapt resource value: 0x7f090000
 			public const int mr_button_content_description = 2131296256;
