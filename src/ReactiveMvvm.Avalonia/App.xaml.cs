b1142afe53cb349a6793afc8d2430a0bdbefa3c1
Artyom <worldbeater-dev@yandex.ru>
2020-09-22 18:01:34 +0300

    Discard the unrelated changes

--- src/ReactiveMvvm.Avalonia/App.xaml.cs
+++ src/ReactiveMvvm.Avalonia/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Avalonia;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
 using ReactiveMvvm.Avalonia.Services;
 using ReactiveMvvm.Avalonia.Views;
 using ReactiveMvvm.Services;
 using ReactiveMvvm.ViewModels;
+using Serilog;
 
 namespace ReactiveMvvm.Avalonia
 {
@@ -17,6 +19,12 @@ namespace ReactiveMvvm.Avalonia
 
         public override void OnFrameworkInitializationCompleted()
         {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
             var view = new FeedbackView();
             var context = new FeedbackViewModel(
                 new AvaloniaSender(view),
