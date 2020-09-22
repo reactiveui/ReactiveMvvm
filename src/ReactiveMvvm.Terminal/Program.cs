b60c61ec4910d669fdc6cd9149e8f613f62f36db
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2020-09-22 18:18:22 +0300

    Add Terminal.Gui and ReactiveUI Demo App (#3)
    * Add Terminal.Gui target
    * Use latest Avalonia and adequate check boxes
    * Force redraw on property change
    * Dance with subjects
    * Remove the accidentally added references
    * Discard the unrelated changes

---
+++ src/ReactiveMvvm.Terminal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ReactiveMvvm.Services;
+using ReactiveMvvm.Terminal.Services;
+using ReactiveMvvm.Terminal.Views;
+using ReactiveMvvm.ViewModels;
+using Terminal.Gui;
+
+namespace ReactiveMvvm.Terminal
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Application.Run(
+                new FeedbackView(
+                    new FeedbackViewModel(
+                        new TerminalSender(), 
+                        new Clock())));
+        }
+    }
+}
---
+++ src/ReactiveMvvm.Terminal/Views/FeedbackView.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using ReactiveMvvm.ViewModels;
+using ReactiveUI;
+using Terminal.Gui;
+using NStack;
+
+namespace ReactiveMvvm.Terminal.Views
+{
+    public sealed class FeedbackView : Window, IViewFor<FeedbackViewModel>, IDisposable
+    {
+        private readonly CompositeDisposable _subscriptions = new CompositeDisposable();
+        
+        public FeedbackView(FeedbackViewModel viewModel) : base(new Rect(0, 0, 30, 20), "Feedback Form")
+        {
+            ViewModel = viewModel;
+            ViewModel.Activator.Activate();
+            this.StackPanel(TimeElapsedLabel())
+                .Append(new Label("Issue Title"))
+                .Append(TitleDescription())
+                .Append(TitleInput())
+                .Append(new Label("Issue Description"))
+                .Append(MessageDescription())
+                .Append(MessageInput())
+                .Append(new Label("Feedback Type"))
+                .Append(IssueCheckBox())
+                .Append(IdeaCheckBox())
+                .Append(new Label("Feedback Category"))
+                .Append(SectionRadioGroup())
+                .Append(new Button("Send Feedback"), 4);
+        }
+
+        private RadioGroup SectionRadioGroup()
+        {
+            var radioGroup = new RadioGroup(new[] {"User Interface", "Audio", "Video", "Voice"});
+            this.WhenAnyValue(x => x.ViewModel.Section)
+                .BindTo(radioGroup, x => x.Selected)
+                .DisposeWith(_subscriptions);
+
+            var selected = new Subject<int>().DisposeWith(_subscriptions);
+            radioGroup.SelectionChanged = index => selected.OnNext(index);
+            selected.AsObservable()
+                .BindTo(this, x => x.ViewModel.Section)
+                .DisposeWith(_subscriptions);
+            return radioGroup;
+        }
+        
+        private CheckBox IssueCheckBox()
+        {
+            var item = new CheckBox("Issue", ViewModel.Issue);
+            this.WhenAnyValue(x => x.ViewModel.Issue)
+                .BindTo(item, x => x.Checked)
+                .DisposeWith(_subscriptions);
+            item.Events()
+                .Toggled
+                .Select(args => item.Checked)
+                .BindTo(this, x => x.ViewModel.Issue)
+                .DisposeWith(_subscriptions);
+            this.WhenAnyValue(x => x.ViewModel.Idea)
+                .Skip(1)
+                .Subscribe(args => item.Redraw(item.Frame))
+                .DisposeWith(_subscriptions);
+            return item;
+        }
+
+        private CheckBox IdeaCheckBox()
+        {
+            var item = new CheckBox("Suggestion", ViewModel.Idea);
+            this.WhenAnyValue(x => x.ViewModel.Idea)
+                .BindTo(item, x => x.Checked)
+                .DisposeWith(_subscriptions);
+            item.Events()
+                .Toggled
+                .Select(old => item.Checked)
+                .BindTo(this, x => x.ViewModel.Idea)
+                .DisposeWith(_subscriptions);
+            this.WhenAnyValue(x => x.ViewModel.Issue)
+                .Skip(1)
+                .Subscribe(args => item.Redraw(item.Frame))
+                .DisposeWith(_subscriptions);
+            return item;
+        }
+
+        private Label TimeElapsedLabel()
+        {
+            var label = new Label("0 seconds passed");
+            this.WhenAnyValue(x => x.ViewModel.Elapsed)
+                .Select(elapsed => (ustring) $"{elapsed} seconds passed")
+                .BindTo(label, x => x.Text)
+                .DisposeWith(_subscriptions);
+            return label;
+        }
+
+        private TextField MessageInput()
+        {
+            var text = new TextField(ViewModel.Message);
+            this.WhenAnyValue(x => x.ViewModel.Message)
+                .BindTo(text, x => x.Text)
+                .DisposeWith(_subscriptions);
+            text.Events()
+                .Changed
+                .Select(older => text.Text)
+                .DistinctUntilChanged()
+                .BindTo(ViewModel, x => x.Message)
+                .DisposeWith(_subscriptions);
+            return text;
+        }
+
+        private Label MessageDescription()
+        {
+            var label = new Label($"0 letters used from {ViewModel.MessageLengthMax}");
+            this.WhenAnyValue(x => x.ViewModel.MessageLength, x => x.ViewModel.MessageLengthMax)
+                .Select(values => (ustring) $"{values.Item1} letters used from {values.Item2}")
+                .BindTo(label, x => x.Text)
+                .DisposeWith(_subscriptions);
+            return label;
+        }
+        
+        private TextField TitleInput()
+        {
+            var text = new TextField(ViewModel.Title);
+            this.WhenAnyValue(x => x.ViewModel.Title)
+                .BindTo(text, x => x.Text)
+                .DisposeWith(_subscriptions);
+            text.Events()
+                .Changed
+                .Select(older => text.Text)
+                .DistinctUntilChanged()
+                .BindTo(ViewModel, x => x.Title)
+                .DisposeWith(_subscriptions);
+            return text;
+        }
+
+        private Label TitleDescription()
+        {
+            var label = new Label($"0 letters used from {ViewModel.TitleLengthMax}");
+            this.WhenAnyValue(x => x.ViewModel.TitleLength, x => x.ViewModel.TitleLengthMax)
+                .Select(values => (ustring) $"{values.Item1} letters used from {values.Item2}")
+                .BindTo(label, x => x.Text)
+                .DisposeWith(_subscriptions);
+            return label;
+        }
+        
+        public FeedbackViewModel ViewModel { get; set; }
+        
+        public void Dispose() => _subscriptions.Dispose();
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (FeedbackViewModel) value;
+        }
+    }
+}
