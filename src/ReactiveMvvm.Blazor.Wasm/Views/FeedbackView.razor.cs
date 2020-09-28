73e06b6bd89f4842aab39f30372c4e7e3f168e21
Artyom <worldbeater-dev@yandex.ru>
2020-09-28 17:19:32 +0300

    Add Blazor.Wasm project

---
+++ src/ReactiveMvvm.Blazor.Wasm/Views/FeedbackView.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using ReactiveMvvm.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Blazor;
+
+namespace ReactiveMvvm.Blazor.Wasm.Views
+{
+    public partial class FeedbackView : ReactiveComponentBase<FeedbackViewModel>
+    {
+        public FeedbackView() => this.WhenActivated(disposables => { });
+
+        [Inject]
+        public FeedbackViewModel Dependency
+        {
+            get => ViewModel;
+            set => ViewModel = value;
+        }
+    }
+}
